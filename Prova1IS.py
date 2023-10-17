d315e48e46d48e07d9d66c5bbbe8b6dc329f9601
CinzaFalso <cnmizerani@hotmail.com>
2023-10-17 17:16:15 -0300

    Adding first test

---
+++ Prova1IS.py
@@ -0,0 +1,19 @@
+var_string = 'Oi'
+print(type(var_string))
+
+var_inteiro = 22
+print(type(var_inteiro))
+
+var_float = 102.60
+print(type(var_float))
+
+var_booleano = True
+print(type(var_booleano))
+
+######################
+
+nome = input('Digite seu nome aqui > ')
+
+######################
+
+print(f'Obrigado pela atenção, {nome}!')
