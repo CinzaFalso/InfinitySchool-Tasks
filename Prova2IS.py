4e58f5ce1ef2ce68865a7077beb78f0ba61c7b89
CinzaFalso <cnmizerani@hotmail.com>
2023-10-25 14:51:09 -0300

    Adding fool-proofing comment to test #2

--- Prova2IS.py
+++ Prova2IS.py
@@ -1,3 +1,13 @@
 velocidade = int(input('Por favor, digite a velocidade do carro: '))
+
 if velocidade > 80:
     print(f'O usuário do carro foi multado, a multa é: R$ {(velocidade - 80) * 20}')
+
+# inpoot = input(': ')
+
+# if not inpoot.isdigit():
+#     print('Erro, não é um dígito')
+# else:
+#     inpoot = int(inpoot)
+#     whatever = (inpoot - 80) * 20
+#     print(whatever)
